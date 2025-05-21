950800de1794f3b3753ee39ee7d7b5505d5177df
Rex <rexnaque31@gmail.com>
2025-05-22 00:57:41 +0800

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function showSurprise() {
-    document.getElementById("surpriseBox").style.display = "block";
-  
-    for (let i = 0; i < 30; i++) {
-      let heart = document.createElement("div");
-      heart.className = "heart";
-      heart.innerHTML = "❤️";
-      heart.style.left = Math.random() * window.innerWidth + "px";
-      heart.style.top = Math.random() * window.innerHeight + "px";
-      heart.style.fontSize = Math.random() * 30 + 20 + "px";
-      document.body.appendChild(heart);
-  
-      setTimeout(() => heart.remove(), 4000);
-    }
-  }
-  
+function showMessage() {
+  alert("I love you more than words can say 💖😘");
+}
